0925def8b5e3fe960c55ffffb6eb867b0b9c3b60
Marcos Silva <marcos.silva79@gmail.com>
2020-08-18 09:44:06 -0400

    feature: created ConnectionsControllers class, addes cors library, refactored route file

--- src/routes.ts
+++ src/routes.ts
@@ -1,18 +1,23 @@
 import express, { response } from 'express';
 import LessonsController  from './controllers/LessonsController';
+import ConnectionsController from './controllers/ConnectionsController';
 import { SchemaBuilder } from 'knex';
 import db from './database/connection';
 
 
 const routes = express.Router();
 
-const lessonController = new LessonsController();
+const lessonsController = new LessonsController();
+const connectionsController = new ConnectionsController();
 
 routes.get('/ping', (req, resp) => {
   return resp.send('pong');
 });
 
-routes.post('/lessons', lessonController.create);
-routes.get('/lessons', lessonController.index);
+routes.get('/lessons', lessonsController.index);
+routes.post('/lessons', lessonsController.create);
+
+routes.get('/connections', connectionsController.index);
+routes.post('/connections', connectionsController.create);
 
 export default routes;
