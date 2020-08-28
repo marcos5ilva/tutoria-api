600aef7cdb9cdc456f9a0df006c624ebd3cea5f2
Marcos Silva <marcos.silva79@gmail.com>
2020-08-28 07:43:35 -0400

    feat: fix server ts route acess

--- src/server.ts
+++ src/server.ts
@@ -3,8 +3,10 @@ import cors from 'cors';
 import routes from './routes';
 
 const app = express();
-app.use(cors);
 app.use(express.json());
 app.use(routes);
+app.use(cors);
+
+
 
 app.listen(3333,()=>{console.log(`server running at port: 3333`)});
