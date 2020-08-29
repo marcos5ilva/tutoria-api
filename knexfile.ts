ef1cb71e2fcd6aabec21a22e4beeca7f09f24a9d
Marcos Silva <marcos.silva79@gmail.com>
2020-08-29 08:48:08 -0400

    chore: add postgres db

--- knexfile.ts
+++ knexfile.ts
@@ -1,10 +1,22 @@
 import path from 'path';
+import dotenv from 'dotenv';
 
-module.exports = {
+dotenv.config();
+
+/*module.exports = {
   client: 'sqlite3',
   connection: {
     filename: path.resolve(__dirname, 'src/database/database.sqlite'),
   },
+  migrations: {
+    directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
+  },
+  useNullAsDefault: true,
+};*/
+
+module.exports = {
+  client: 'pg',
+  connection: process.env.PG_CONNECTION_STRING,
   migrations: {
     directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
   },
--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,12 +1,24 @@
 import knex from 'knex';
 import path from 'path';
+import dotenv from "dotenv";
+import configuration from '../../knexfile';
+dotenv.config();
 
-const db = knex({
+
+
+/*const db = knex({
   client: 'sqlite3',
   connection: {
     filename: path.resolve(__dirname, 'database.sqlite'),
   },
   useNullAsDefault: true,
-});
+});*/
+
+const db = knex({
+  client: 'pg',
+  connection: process.env.PG_CONNECTION_STRING,
+  searchPath: ['knex', 'public'],
+  useNullAsDefault: true,
+})
 
 export default db;
